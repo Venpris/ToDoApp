76bab224be3c9512ea85dbf600380c55f3b6e962
Sebastian Carman <sebastiancarman.sc@gmail.com>
2025-04-05 00:41:09 -0400

    Fix button and adapter

--- app/src/main/java/com/example/todoapp/CategoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/todoapp/CategoryRecyclerViewAdapter.kt
@@ -4,11 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoryRecyclerViewAdapter(private val categoryList: List<Category>): RecyclerView.Adapter<CategoryRecyclerViewAdapter.CategoryViewHolder>() {
     inner class CategoryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val button: Button = itemView.findViewById(R.id.category_button)
+        val icon: ImageView = itemView.findViewById(R.id.category_icon)
     }
 
     override fun onCreateViewHolder(
@@ -28,15 +31,11 @@ class CategoryRecyclerViewAdapter(private val categoryList: List<Category>): Rec
 
         if (category.isStarredCategory) {
             holder.button.text = "" // Clear text
-            holder.button.setCompoundDrawablesWithIntrinsicBounds(
-                R.drawable.ic_star_filled, 0, 0, 0
-            )
+            holder.icon.visibility = View.VISIBLE
             holder.button.contentDescription = context.getString(R.string.ic_star_desc)
         } else {
             holder.button.text = category.name
-            holder.button.setCompoundDrawablesWithIntrinsicBounds(
-                0, 0, 0, 0
-            )
+            holder.icon.visibility = View.GONE
             holder.button.contentDescription = ""
         }
     }
