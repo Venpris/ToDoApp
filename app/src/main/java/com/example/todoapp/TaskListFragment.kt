f1e4f760a69fb94fe8a4f99b00f55bca5aee2fe9
Sebastian Carman <sebastiancarman.sc@gmail.com>
2025-04-05 15:28:27 -0400

    Create task list fragment (only displays header right now), use test data to populate header

--- app/src/main/java/com/example/todoapp/TaskListFragment.kt
+++ app/src/main/java/com/example/todoapp/TaskListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,17 +19,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TaskListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var categoryRecyclerView: RecyclerView
+    private lateinit var categoryRecyclerViewAdapter: CategoryRecyclerViewAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val categoryList = listOf(
+        Category(1, "Starred", null, true),
+        Category(2, "My Tasks", null, false),
+        Category(3, "Work", null, false),
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +36,12 @@ class TaskListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_task_list, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TaskListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TaskListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        categoryRecyclerView = view.findViewById(R.id.category_recycler_view)
+        categoryRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        categoryRecyclerViewAdapter = CategoryRecyclerViewAdapter(categoryList)
+        categoryRecyclerView.adapter = categoryRecyclerViewAdapter
     }
 }
