12f11b5f32dfeb2998e4dd936352a80a63d52b0b
Sebastian Carman <sebastiancarman.sc@gmail.com>
2025-04-05 15:30:49 -0400

    Remove dumb comments

--- app/src/main/java/com/example/todoapp/TaskListFragment.kt
+++ app/src/main/java/com/example/todoapp/TaskListFragment.kt
@@ -8,16 +8,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TaskListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TaskListFragment : Fragment() {
     private lateinit var categoryRecyclerView: RecyclerView
     private lateinit var categoryRecyclerViewAdapter: CategoryRecyclerViewAdapter
