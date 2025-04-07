af2db9e3d1b0d5b7c9070dfcfda1cf34b8b0c68b
Sebastian Carman <sebastiancarman.sc@gmail.com>
2025-04-07 01:16:02 -0400

    Add dao methods

--- app/src/main/java/com/example/todoapp/CategoryDao.kt
+++ app/src/main/java/com/example/todoapp/CategoryDao.kt
@@ -1,10 +1,22 @@
 package com.example.todoapp
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface CategoryDao {
     @Query("SELECT * FROM categories")
     fun getAll(): List<Category>
+
+    @Insert
+    fun insertAll(vararg categories: Category)
+
+    @Delete
+    fun deleteCategories(vararg categories: Category)
+
+    @Update
+    fun updateCategories(vararg categories: Category)
 }
--- app/src/main/java/com/example/todoapp/TaskDao.kt
+++ app/src/main/java/com/example/todoapp/TaskDao.kt
@@ -1,10 +1,31 @@
 package com.example.todoapp
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface TaskDao {
-    @Query("SELECT * FROM tasks")
+    @Query("SELECT * FROM tasks WHERE isDone = 0")
     fun getAll(): List<Task>
+
+    @Query("SELECT * FROM tasks WHERE categoryId = :categoryId AND isDone = 0")
+    fun filterTasksByCategory(categoryId: Int): List<Task>
+
+    @Query("SELECT * FROM tasks WHERE isDone = 1")
+    fun getDoneTasks(): List<Task>
+
+    @Query("SELECT * FROM tasks WHERE isStarred = 1 AND isDone = 0")
+    fun getStarredTasks(): List<Task>
+
+    @Insert
+    fun insertAll(vararg tasks: Task)
+
+    @Delete
+    fun deleteTasks(vararg tasks: Task)
+
+    @Update
+    fun updateTasks(vararg tasks: Task)
 }
